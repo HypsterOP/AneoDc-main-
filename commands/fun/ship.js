46d1033cf70248a40344902959e23bbffe1ad991
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-05 17:01:00 +0530

    ship command

---
+++ commands/fun/ship.js
@@ -0,0 +1,36 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const { random } = require('mathjs');
+
+module.exports = {
+    name: 'ship',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        let user = message.mentions.users.first()
+        let Random = Math.floor(Math.random() * 100) + 1
+
+        if(!user) return message.channel.send(`Please tell me the user whom you want to ship! UwU`)
+        if(user === message.author) return message.channel.send("You cannot ship yourself")
+        
+        const unloveEmbed = new MessageEmbed()
+        .setTitle(`Looks like it isn't a great match for both of them 💔`)
+        .setThumbnail(`https://cdn.discordapp.com/attachments/824893988384407592/828590284907872256/iu.png`)
+        .setDescription(`${message.author} was shipped with ${user} and the percentage was ${Random}% :sob:`)
+        .setColor("RED")
+
+        const loveEmbed = new MessageEmbed()
+        .setTitle(`They both are born for each other ♥`)
+        .setThumbnail(`https://cdn.discordapp.com/attachments/824893988384407592/828591233218904104/iu.png`)
+        .setDescription(`${message.author} was shipped with ${user} and the percentage was ${Random}% 😄`)
+        .setColor('GREEN')
+
+        if(Random > 50) {
+            message.channel.send(loveEmbed)
+        } else {
+            message.channel.send(unloveEmbed)
+        }
+    }
+}
