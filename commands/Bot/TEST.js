0b1ffd63496bf462f8c9be98e7e7b5f825467501
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-07 14:41:58 +0530

    added

---
+++ commands/Bot/TEST.js
@@ -0,0 +1,18 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'test',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(message.author.id != "800331322089537538") return;
+
+        message.reply("test command working hypster", { allowedMentions: 
+            { 
+                repliedUsers: false 
+            }})
+    }
+}
