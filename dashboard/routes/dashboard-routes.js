d1e5a15ec05323d68a449edddd02799176cd2db7
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-12 21:55:43 +0530

    idk LMAO

--- dashboard/routes/dashboard-routes.js
+++ dashboard/routes/dashboard-routes.js
@@ -1,9 +1,12 @@
+const client = require("../../index")
 const express = require('express')
 
 const router = express.Router();
 
 router.get('/dashboard', (req, res) => res.render('dashboard/index'));
 
-router.get('/servers/:id', (req, res) => res.render('dashboard/index'));
+router.get('/servers/:id', (req, res) => res.render('dashboard/index', {
+  guild : client.guilds.cache.get(req.params.id)
+}));
 
 module.exports = router;
--- dashboard/server.js
+++ dashboard/server.js
@@ -22,5 +22,5 @@ middleware.validateUser , middleware.updateGuilds, dashboardRoutes);
 
 app.get('*', (req, res) => res.render('errors/404'));
 
-const port = 5555;
+const port = 3000;
 app.listen(`${port}`, () => console.log(`Server is ready on port ${port}, http://localhost:${port}`));
