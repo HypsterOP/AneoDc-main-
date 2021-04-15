d0d7baf9d05f3a2835fe3c2f6b58e38a0c161fa3
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-15 14:37:07 +0530

    ok lo

--- dashboard/modules/auth-client.js
+++
@@ -1,8 +0,0 @@
-const OAuthClient = require('disco-oauth');
-const config = require('../../config.json');
-
-const client = new OAuthClient(config.id, config.secret);
-client.setRedirect(`${config.dashboardURL}/auth`);
-client.setScopes('identify', 'guilds');
-
-module.exports = client;
