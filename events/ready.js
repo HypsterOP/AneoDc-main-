ba538d43df9b8bb886c23aaa64957c974f41f9ae
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-25 16:59:26 +0530

    asd

--- events/ready.js
+++ events/ready.js
@@ -10,26 +10,6 @@ client.on('ready', () =>{
   type: "STREAMING",
   url: "https://www.youtube.com/channel/UCjGTZFB1kvFi299j-FlSXaQ"
   });
-        setInterval(() => {
-        Schema.find().then((data) => {
-            if(!data && data.length) return;
-
-            data.forEach((value) => {
-                const guild = client.guilds.cache.get(value.Guild);
-                const memberCount = guild.memberCount;
-
-                if(value.Member != memberCount) {
-                    console.log("The membercount differs")
-                    const channel = guild.channels.cache.get(value.Channel);
-                    channel.setName(`Members: ${memberCount}`)
-
-                    value.Member = memberCount;
-                    value.save();
-                }
-        });
-    });
-
-}, ms('2 minutes'));
 
 
 });
