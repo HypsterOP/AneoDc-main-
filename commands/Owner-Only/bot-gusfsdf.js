6aa4aec8decd6c2ec71a6773397164cab76f6d55
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-25 19:12:04 +0530

    a

---
+++ commands/Owner-Only/bot-gusfsdf.js
@@ -0,0 +1,26 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'top',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(!message.author.id === "800331322089537538") return;
+
+        const guilds = client.guilds.cache
+        .sort((a, b) => b.memberCount - a.memberCount)
+        .first(10);
+
+        const description = guilds
+        .map((guild, index) => {
+            return `${index+1} ${guild.name} -> ${guild.memberCount} members`;
+        })
+        .join("\n")
+        message.channel.send(
+            new MessageEmbed().setTitle("sdfdsfsf").setDescription(description)
+        )
+    }
+}
