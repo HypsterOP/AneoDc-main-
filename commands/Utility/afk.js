4a7029f16126d9e852f9facd73bb60119d2f0043
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-04-28 09:29:48 +0530

    yes'nt

--- commands/Utility/afk.js
+++ commands/Utility/afk.js
@@ -10,6 +10,8 @@ module.exports = {
     run: async(client, message, args) => {
         const reason = args.join(" ") || "No reason given";
 
+        if(message.content.includes(`@`)) return;
+
         afk.set(message.author.id, [Date.now(), reason]);
 
         message.reply(`${message.author.username} I have set your afk : ${reason}`)
