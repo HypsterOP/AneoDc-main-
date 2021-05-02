e4d81b272a805d43724807986bf0ac95656888b3
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-02 12:40:07 +0530

    OOOOOF

--- commands/Economy/buy.js
+++ commands/Economy/buy.js
@@ -1,5 +1,5 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
-const inventory = require("../../models/inventory")
+const inventory = require('../../models/inventory');
 const items = require("../../shopItems")
 module.exports = {
     name: 'buy',
@@ -9,19 +9,18 @@ module.exports = {
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-        if(!args[0]) return message.reply("What do u want to buy lol")
-        const itemToBuy = args[0].toLowerCase();
+        if(!args[0]) return message.reply(`What do u want to buy lol`)
+        const itemToBuy = args[0].toLowerCase()
 
         const validItem = !!items.find((val) => val.item.toLowerCase() === itemToBuy);
-        if(!validItem) return message.reply(`That Item doesn't exist lol`)
+        if(!validItem) return message.reply(`That item doesn't exist lol`);
 
         const itemPrice = items.find((val) => (val.item.toLowerCase()) === itemToBuy).price;
 
         const userBalance = await client.bal(message.author.id);
-        if(userBalance < itemPrice) return message.reply(`Lol you only have ${userBalance}, the price of the item is ${itemPrice} coins`)
+        if(userBalance < itemPrice) return message.reply(`LoL you only have ${userBalance}, the price of that item is ${itemPrice} coins!`)
 
         const params = {
-            Guild: message.guild.id,
             User: message.author.id
         }
         inventory.findOne(params, async(err, data) => {
@@ -32,19 +31,18 @@ module.exports = {
                 } else {
                     data.Inventory[itemToBuy]++
                 }
-                console.log(data);
+                console.log(data)
                 await inventory.findOneAndUpdate(params, data);
             } else {
                 new inventory({
-                    Guild: message.guild.id,
                     User: message.author.id,
                     Inventory: {
                         [itemToBuy]: 1
-                    },
+                    }
                 }).save();
             }
             message.reply(`You have bought ${itemToBuy}`);
-            client.rmv(message.author.id, itemPrice)
-        });
-    },
-};
+            client.rmv(message.author.id, itemPrice);
+        })
+    }
+}
--- commands/Economy/inventory.js
+++ commands/Economy/inventory.js
@@ -1,30 +1,24 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
 const inventory = require("../../models/inventory")
-const items = require("../../shopItems")
 module.exports = {
     name: 'inventory',
-    aliases: ["inv"],
+    aliases: ['inv'],
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-        inventory.findOne({
-            Guild: message.guild.id,
-            User: message.author.id
-        }, async (err, data) => {
-            if(!data) return message.channel.send(`The inventory is empty`)
+        inventory.findOne({ User: message.author.id }, async(err, data) => {
+            if(!data) return message.channel.send(`The inventory is empty!`)
             const mappedData = Object.keys(data.Inventory).map((key) => {
                 return `${key}(${data.Inventory[key]})`
-            }).join("\n")
-
+            }).join("\n\n");
             message.channel.send(
                 new MessageEmbed()
+                .setTitle(`Inventory`)
                 .setDescription(mappedData)
-                .setColor("RANDOM")
             )
-        }
-        )
-    },
-};
+        });
+    }
+}
