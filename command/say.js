a408ce50cbc3c8b39605b0cda3747f12027c23a5
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-02 19:45:34 +0530

    slash command 'say'

---
+++ command/say.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    slash: true,
+    testOnly: true,
+    description: "Says whatever you type only as an embed description, also color must be in capital letters!",
+    minArgs: 2,
+    expectedArgs : "<word> <color>",
+
+    callback: ({ args }) => {
+        const embed = new MessageEmbed()
+        
+        const [word, color] = args
+
+        embed.setDescription(word)
+        embed.setColor(color || "RANDOM")
+
+        return embed
+    }
+}
