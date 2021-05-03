f9a74b94d59dc386014245d2942e6232aa4477aa
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-03 17:41:49 +0530

    auto-rol

--- commands/Utility/autorolecheck.js
+++ commands/Utility/autorolecheck.js
@@ -12,6 +12,6 @@ module.exports = {
         const check = await db.has(`autorole-${message.guild.id}`);
         if(check === false) return message.reply('There is no autorole set for this guild!');
         const role = await db.get(`autorole-${message.guild.id}`);
-        message.reply(`The autorole for ${message.guild.name} is <@&${role.name}>`);
+        message.reply(`The autorole id for ${message.guild.name} is ${role}`);
     }
 }
