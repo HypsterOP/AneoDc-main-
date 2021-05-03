08cf243776dca19532541d118e36043a9cdde352
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-03 09:41:22 +0530

    idk bruh top.gg mc

--- commands/antiswear/anticurse-disable.js
+++ commands/antiswear/anticurse-disable.js
@@ -3,7 +3,7 @@ const db = require('../../reconDB');
 module.exports = {
     name: 'antiswear-off',
     run : async(client, message, args) => {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return;
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
         if(!message.guild.me.hasPermission('MANAGE_MESSAGES')) return message.channel.send('I do not have permission to delete messages.')
 
         if(await db.has(`swear-${message.guild.id}`) === true) {
--- commands/moderation/warns.js
+++ commands/moderation/warns.js
@@ -7,7 +7,7 @@ module.exports = {
      * @param {Message} message
      */
     run : async(client, message, args) => {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return;
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
         const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         if(!user) return message.channel.send('User not found.')
         const reason = args.slice(1).join(" ")
