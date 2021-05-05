f51fd636c2ae2847d83f0282b3ba05cab3f841b9
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-05 16:36:25 +0530

    new thingy

--- commands/fun/mock.js
+++ commands/fun/mock.js
@@ -1,21 +1,21 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 module.exports = {
-	name: 'mock',
-	aliases: ['mo'],
-	/**
-	 * @param {Client} client
-	 * @param {Message} message
-	 * @param {String[]} args
-	 */
-	run: async (client, message, args) => {
-		const text = args.join("  ");
-		if (!text) return message.reply('Please enter a text');
+    name: 'mock',
+    aliases: ['mo'],
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        const text = args.join("  ");
+        if (!text) return message.reply('Please enter a text');
 
-		const data = await fetch(
-            `https://hypeapi.herokuapp.com/mock/${text}`
+        const data = await fetch(
+            `https://hypsterapi.herokuapp.com/mock/${text}`
             ).then((res) => res.json());
 
         message.channel.send(data.response)
-	},
+    },
 };
