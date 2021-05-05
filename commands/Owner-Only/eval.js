8d0b4d8ecd7eb7f3b277b13f0bc32ccde4c6c180
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-05 21:35:49 +0530

    better eval cmd

--- commands/Owner-Only/eval.js
+++ commands/Owner-Only/eval.js
@@ -1,60 +1,46 @@
-const { Client, Message, MessageEmbed } = require('discord.js'),
-  { post } = require('node-superfetch');
-  const config = require('../../config.json')
+const { Client, Message, MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'eval',
+    aliases: [''],
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-      if (message.author.id !== "800331322089537538") return;
+      if (message.author.id !== '800331322089537538') return;
       const embed = new MessageEmbed()
-      .addField("Input", "```js\n" + args.join(" ") + "```");
-
+          .setTitle('Evaluating...')
+      const msg = await message.channel.send(embed);
       try {
-        const code = args.join(" ")
-        if(!code) return message.channel.send("Hypster noob give me the code ");
-        let evaled;
-        if (code.includes(`SECRET`) || code.includes(`TOKEN`) || code.includes("process.env")) {
-        evaled = "Check the config.json dumb"
-        } else {
-          evaled = eval(code);
+          const data = eval(args.join(' ').replace(/```/g, ''));
+          const embed = new MessageEmbed()
+              .setTitle('Output: ')
+              .setDescription(await data)
+              .setColor('RANDOM')
+          await msg.edit(embed)
+          await msg.react('✅')
+          await msg.react('❌')
+          const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
+          msg.awaitReactions(filter, { max: 1 })
+              .then((collected) => {
+                  collected.map((emoji) => {
+                      switch (emoji._emoji.name) {
+                          case '✅':
+                              msg.reactions.removeAll();
+                              break;
+                          case '❌':
+                              msg.delete()
+                              break;
+                      }
+                  })
+              })
+      } catch (e) {
+          const embed = new MessageEmbed()
+              .setTitle('An Error has occured')
+          return await msg.edit(embed);
 
-        }
-
-        if (typeof evaled !== "string") eval = require("util").inspect(evaled, {depth: 0})
-
-        let output = clean(evaled)
-        if (output.length > 1024) {
-          const {body} = await post("https://hastebin.com/documents").send(output);
-          embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor(0x7289DA)
-        } else {
-          embed.addField("Output", "```js\n" + output + "```").setColor(0x7289DA)
-        }
-
-        message.channel.send(embed)
-
-      } catch (error) {
-        let err = clean(error)
-        if (err.length > 1024) {
-          const {body} = await post("https://hastebin.com/documents").send(err);
-          embed.addField("Output", `https://hastebin.com/${body.key}.js`).setColor("RED")
-        } else {
-          embed.addField("Output", "```js\n" + err + "```").setColor("RED");
-        }
-
-        message.channel.send(embed)
       }
     }
-}
-
-function clean(string) {
-  if (typeof text === "string") {
-    return string.replace(/`/g, "`" + String.fromCharCode(8203))
-    .replace(/@/g, "@" + String.fromCharCode(8203))
-  } else {
-    return string;
-  }
 }
