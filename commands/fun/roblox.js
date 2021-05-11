63e6de2647b3b7ec718b2b0559305079dea213f8
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-11 08:05:46 +0530

    roblox.png

---
+++ commands/fun/roblox.js
@@ -0,0 +1,25 @@
+const Meme = require("memer-api")
+const memer = new Meme();
+const { Client, Message, MessageEmbed, MessageAttachment } = require('discord.js');
+
+module.exports = {
+    name: 'roblox',
+    usage: 'roblox @<user>',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        const user = message.mentions.users.first();
+
+        if(!user) return message.reply(`You forgot to mention someone!`)
+
+        const av = user.displayAvatarURL()
+
+        memer.roblox(av).then(image => {
+            const attach = new MessageAttachment(image, "roblox.png")
+            message.channel.send(attach)
+        })
+    }
+}
