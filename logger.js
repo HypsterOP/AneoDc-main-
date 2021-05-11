fb7f8ba61ac29fcc40b9b04706cf42769a7db4cc
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-11 09:55:22 +0530

    pushed logger into 'main'

--- logger.js
+++ logger.js
@@ -31,7 +31,7 @@ module.exports = c => {
         "YELLOW",
         "Channel PINS UPDATE",
         `Channel Name: \`${channel.name}\`\nChannelID: \`${channel.id}\`\nPinned at \`${time}\``,
-        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/samsung/265/pushpin_1f4cc.png"
+        "https://imgur.com/eG2Kpk3"
       );
     });
     c.on("channelUpdate", function(oldChannel, newChannel) {
