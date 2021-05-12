2ad9439e08fb4f4aae9cea0db1e815b052b43a9b
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-12 12:22:57 +0530

    changed status

--- events/ready.js
+++ events/ready.js
@@ -22,14 +22,32 @@ client.on('ready', () =>{
       })
     })
 
-    client.user.setPresence({
-      status: 'online',
-      activity: {
-          name: "h!help | Shard #0",
-          type: 'WATCHING',    
-      }
+    const timeModels = [
+      "11 Hours",
+      "10 Hours",
+      "9 hours",
+      "8 Hours",
+      "7 Hours",
+      "6 hours",
+      "5 Hours",
+      "4 Hours",
+      "3 Hours",
+      "2 Hours",
+      "1 Hour"
+    ]
 
-    
-  })
+    setInterval(function() {
+
+      client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: `h!help | Happy Birthday TechInfinty(CEO) and Discord in ${timeModels}`,
+            type: 'STREAMING',
+            url: "https://twitch.tw/discord"    
+        }
+  
+      
+    })
+    }, 3600000)
 
 });
