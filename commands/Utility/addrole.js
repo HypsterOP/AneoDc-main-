a781af748b5026bad84c94f755d0a276e125db09
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-15 21:19:00 +0530

    addrole XD

--- commands/Utility/addrole.js
+++ commands/Utility/addrole.js
@@ -1,24 +1,47 @@
-const { Message, MessageEmbed } = require('discord.js')
-const config = require("../../config.json")
+const { Client, Message, MessageEmbed } = require('discord.js');
+
 module.exports = {
-    name : 'addrole',
-    run : async(client, message, args) => {
-        //lets use parameters (optional)
-        /**
-         * @param {Message} message
-         */
-        
+    name: 'addrole',
+    description: 'Add role to a user !',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_ROLES")) return;
-        
-        const target = message.mentions.members.first()
-        if(!target) return message.channel.send(`I couldn\'t find that member, or there was no member ${config.femoji} `) //when no member is pinged
-        const role = message.mentions.roles.first()
-        if(!role) return message.channel.send('please mention a role')
-        
-        await target.roles.add(role)
-        message.channel.send(new MessageEmbed().setDescription(`${config.semoji} Added roles to ${target.user.username}`))
-        .catch(error => {
-            message.channel.send(`An Error Occured Make sure my roles is above the role which you want to give!`)
-        })
+        const user = message.mentions.members.last() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0])
+
+        const role = message.mentions.roles.first() || message.guild.roles.cache.get(args[1]) || message.guild.roles.cache.find(x => x.name.toLowerCase() === args.slice(1).join(" ") || x.name === args[1])
+
+        if (!user) {
+            return message.channel.send(`You need to specify a valid member`)
+        }
+
+        if (!role) {
+            return message.channel.send(`You need to specify a valid role`)
+        }
+
+        if (message.guild.me.roles.highest.id === role.id) {
+            return message.channel.send(`I cannot add or remove that role because that is my highest role`)
+        }
+
+        if (user.roles.cache.has(role.id)) {
+            try {
+                user.roles.remove(role.id)
+                return message.channel.send(`Removed ${role.name} from ${user.user.tag}`)
+            }
+            catch (e) {
+                return message.channel.send(`There was an error: ${e}`)
+            }
+        } else {
+            try {
+                user.roles.add(role.id)
+                return message.channel.send(`Added ${role.name} to ${user.user.tag}`)
+            }
+            catch (e) {
+                return message.channel.send(`There was an error: ${e}`)
+            }
+        }
     }
 }
