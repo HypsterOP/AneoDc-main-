7ff1b8779fc77b9644fbb01fea981fac1f92ce21
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-18 21:30:46 +0530

    sus

---
+++ commands/info/perms.js
@@ -0,0 +1,41 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const permissions = require("../../utils/permissions.json");
+module.exports = {
+    name: 'permissions',
+    aliases: ['perms'],
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply(`Hey! Sorry but u need **Manage Roles** permission to do that!`)
+        function getChannelFromMention(message, mention) {
+            if (!mention) return;
+            const matches = mention.match(/^<#(\d+)>$/);
+            if (!matches) return;
+            const id = matches[1];
+            return message.guild.channels.cache.get(id);
+        }
+            const member = 
+                  message.mentions.users.first() ||
+                  message.guild.members.cache.get(args[0]) || 
+                  message.member;
+            
+            const memberPermissions = member.permissions.toArray();
+            const finalPermissions = [];
+            for (const permission in permissions) {
+                if (memberPermissions.includes(permission)) finalPermissions.push(`+ ${permissions[permission]}`);
+                else finalPermissions.push(`- ${permissions[permission]}`);
+            }
+            
+            const embed = new MessageEmbed()
+                .setTitle(`${member.displayName}'s Permissions`)
+                .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+                .setDescription(`\`\`\`diff\n${finalPermissions.join("\n")}\`\`\``)
+                .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
+                .setColor('#2F3136')
+                .setTimestamp();
+            message.channel.send(embed);
+    }
+}
