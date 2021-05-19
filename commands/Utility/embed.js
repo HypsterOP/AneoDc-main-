316c867b08baad3e5074855f10c272bfbb342986
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-19 23:38:04 +0530

    better emnbed cmd

--- commands/Utility/embed.js
+++ commands/Utility/embed.js
@@ -1,70 +1,60 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
-const discord = require("discord.js")
+const { prompt } = require("nekoyasui")
 module.exports = {
     name: 'embed',
-    aliases: ['em'],
+    aliases: ['emb'],
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-        let questions = {
-            firstQuestion: "Please type the embed title! it will look like this - https://imgur.com/imSLCsI",
-            secondQuestion: "Please type the embed Description! - it will look like this - https://imgur.com/dZwxlYk",
-            thirdQuestion: "Please type the color! It can be a color like red or a hex code.",
-            fourthQuestion: "Please type the footer - it will look like this - https://imgur.com/QLPzngb",
-        }
-           
-            message.channel.send(questions.firstQuestion).then(msg => {
-                const filter1 = m => m.author.id === message.author.id
-                msg.channel.awaitMessages(filter1, {
-                    time: 5 * 60000,
-                    max: 1
-                }).then(messages => {
-                    let msg1 = messages.first().content
-                    if(msg1.toLowerCase() === "cancel") return message.channel.send("Cancelled.")
-                    message.channel.send(questions.secondQuestion).then(msg => {
-                        const filter1 = m => m.author.id === message.author.id
-                        msg.channel.awaitMessages(filter1, {
-                            time: 5 * 60000,
-                            max: 1
-                        }).then(messages => {
-                            let msg2 = messages.first().content
-                            if(msg2.toLowerCase() === "cancel") return message.author.send("Ok, I have cancelled this process")
-                            message.channel.send(questions.thirdQuestion).then(msg => {
-                                const filter1 = m => m.author.id === message.author.id
-                                msg.channel.awaitMessages(filter1, {
-                                    time: 5 * 60000,
-                                    max: 1
-                                }).then(messages => {
-                                    let msg3 = messages.first().content.toUpperCase()
-                                    if(msg3.toLowerCase() === "cancel") return message.author.send("Ok, I have cancelled this process")
-                                    message.channel.send(questions.fourthQuestion).then(msg => {
-                                        const filter1 = m => m.author.id === message.author.id
-                                        msg.channel.awaitMessages(filter1, {
-                                            time: 5 * 60000,
-                                            max: 1
-                                        }).then(messages => {
-                                            let msg4 = messages.first().content
-                                            if(msg4.toLowerCase() === "cancel") return message.author.send("Ok, I have cancelled this process")
-                                            console.log("Embed created")
-                                                
-                                                        message.channel.send(
-                                                            new discord.MessageEmbed()
-                                                                .setTitle(msg1)
-                                                                .setDescription(msg2)
-                                                                .setColor(msg3)
-                                                                .setFooter(msg4)
-                                                        )
-                                                
-                                                })
-                                            })
-                                        })
-                                    })
-                                })
-                            })
-                        })
-                    })
+        const ask = {
+            title: "OKay, first of all, what do you want to be the embed's title? Type the title into the chat now!",
+            desc: "Type the description that you want in the embed!",
+            color: "Type the color you want to be on the embed! It can be a color like red or a hex code.",
+            footer: "What do you want its footer to be?",
+            note: "To cancel the command type: `cancel` | To skip the question type: `skip`"
+          };
+          
+          var answer = {};
+          answer.cancel = "ok! cancelling the embed..";
+          answer.skip = "skipping the question, proceed to the next question.";
+          
+          answer.title = await prompt.reply(message.channel, ask.title + "\n" + ask.note, { userID: message.author.id });
+          if (answer.title === "skip") {
+            const skip = await message.channel.send(answer.skip);
+            setTimeout(async () => { await skip.delete(); }, 9e3);
+          }
+          if (!answer.title || answer.title === "cancel") return message.channel.send(answer.cancel);
+          
+          answer.desc = await prompt.reply(message.channel, ask.desc + "\n" + ask.note, { userID: message.author.id });
+          if (answer.desc === "skip") {
+            const skip = await message.channel.send(answer.skip);
+            setTimeout(async () => { await skip.delete(); }, 9e3);
+          }
+          if (!answer.desc || answer.desc === "cancel") return message.channel.send(answer.cancel);
+          
+          answer.color = await prompt.reply(message.channel, ask.color + "\n" + ask.note, { userID: message.author.id });
+          if (answer.color === "skip") {
+            const skip = await message.channel.send(answer.skip);
+            setTimeout(async () => { await skip.delete(); }, 9e3);
+          }
+          if (!answer.color || answer.color === "cancel") return message.channel.send(answer.cancel);
+          
+          answer.footer = await prompt.reply(message.channel, ask.footer + "\n" + ask.note, { userID: message.author.id });
+          if (answer.footer === "skip") {
+            const skip = await message.channel.send(answer.skip);
+            setTimeout(async () => { await skip.delete(); }, 9e3);
+          }
+          if (!answer.footer || answer.footer === "cancel") return message.channel.send(answer.cancel);
+          
+          const embed = new MessageEmbed();
+          if (answer.title !== "skip") embed.setTitle(answer.title);
+          if (answer.desc !== "skip") embed.setDescription(answer.desc);
+          if (answer.color !== "skip") embed.setColor(answer.color);
+          if (answer.footer !== "skip") embed.setFooter(answer.footer);
+          
+          return message.channel.send("", { embed : embed });
     }
 }
