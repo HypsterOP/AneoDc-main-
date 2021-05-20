6bddbee982a66cd5ee4c61dcafb3ae7972d02120
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-20 10:47:12 +0530

    better stats cmd

--- commands/Bot/stats.js
+++ commands/Bot/stats.js
@@ -1,4 +1,5 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
+const { mem, cpu } = require("node-os-utils")
 let m = require('moment-duration-format'),
     os = require('os'),
     cpuStat = require('cpu-stat'),
@@ -13,7 +14,7 @@ module.exports = {
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-        cpuStat.usagePercent(function (error, percent, seconds) {
+        cpuStat.usagePercent(async function (error, percent, seconds) {
             if (error) {
               return console.error(error)
             }
@@ -25,9 +26,10 @@ module.exports = {
             const usage = formatBytes(process.memoryUsage().heapUsed)
             const node = process.version
             const CPU = percent.toFixed(2)
+            const { totalMemMb, usedMemMb } = await mem.info()
 
             const embed = new MessageEmbed()
-            .addField('Aneo Stats:', `**<:Server_Owner:838109754098188329> Total Servers** ${guilds}\n\n**<:bfdverifieduser:838109624183816232> Total Users**: ${users}\n\n**<:CH_IconGreyTextChannel:838109379898376193> Total Channels**: ${channels}\n\n**<a:Success:821621580215877644> Usage**: ${usage}\n\n**<:nodejs:838108744092876810> Node Version**: ${node}\n\n**<:discordjs:838285692676735007> Discord.js Version**: v${version1}\n\n**<:cpu:838108865287028756> Cpu Usage** ${CPU}`)
+            .addField('Aneo Stats:', `**<:Server_Owner:838109754098188329> Total Servers** ${guilds}\n\n**<:bfdverifieduser:838109624183816232> Total Users**: ${users}\n\n**<:CH_IconGreyTextChannel:838109379898376193> Total Channels**: ${channels}\n\n**<a:Success:821621580215877644> Usage**: ${usage}\n\n**<:nodejs:838108744092876810> Node Version**: ${node}\n\n**<:discordjs:838285692676735007> Discord.js Version**: v${version1}\n\n**<:cpu:838108865287028756> Cpu Usage** ${CPU}\n\n💻 Total Ram: ${totalMemMb} Mb\n\n📀 Ram used: ${usedMemMb}`)
             .addField('**Cpu Stats**', `**CPU**: ${cpuModel}\n\n **Cores**: ${cores}`)
             .setColor('BLUE')
             .setTimestamp()
