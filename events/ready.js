c8a98ae2f016eb571929fc32b1802eb42ffb5081
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-29 11:26:19 +0530

    top.gg

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,8 @@ const path = require('path');
 const chalk = require("chalk")
 const ms = require('ms');
 const Schema = require('../models/member-count');
+const Topgg = require('@top-gg/sdk')
+const api = new Topgg.Api('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxMTI2NTE5NTE4Njk3ODgyOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjIyMjY3NDI4fQ.7SRva33Av2xaB1_jZ4PSaedpYn_H9rzsO52EKjucvds')
 const WokCommands = require('wokcommands');
 // https://discord.com/api/webhooks/847787309877559306/UrTB3GsKzJq6PIo7WZIISZKNvCYJ8u3OQBp2CEB9q14G5z5CZoNXgCXMYUEboWzZBXtj
 const Schema1 = require('../models/blacklist-word');
@@ -12,6 +14,14 @@ const { BlacklistedWords } = require('../Collection');
 const guildId = '814117890701787157';
 const quick = require('quick.db');
 client.on('ready', () => {
+
+setInterval(() => {
+	api.postStats({
+		serverCount: client.guilds.cache.size,
+		shardId: client.shard.ids[0],
+		shardCount: client.options.shardCount
+	})
+}, 1800000)
 
 	const hook = new dc.WebhookClient('847787309877559306', 'UrTB3GsKzJq6PIo7WZIISZKNvCYJ8u3OQBp2CEB9q14G5z5CZoNXgCXMYUEboWzZBXtj')
 
