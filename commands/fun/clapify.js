520af7ac5d054a4399ffecd1507ba61de8b997e3
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-30 12:50:00 +0530

    clapify

---
+++ commands/fun/clapify.js
@@ -0,0 +1,25 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'clapify',
+	aliases: ['calp'],
+	description: ':clap: Clapify your text',
+	usage: 'clapify <text>',
+	/**
+	 * @param {Client} client
+	 * @param {Message} message
+	 * @param {String[]} args
+	 */
+	run: async (client, message, args) => {
+		if (!args[0])
+			return message.channel.send(
+				`:clap: **|** :clap: How :clap: can :clap: I :clap: clapify :clap: that :clap: message? :clap:`
+			);
+
+		const text = args.join(' :clap: ');
+        
+        if(text.length > 100) return message.channel.send(`:clap: **|** :clap: That's :clap: over :clap: the :clap: limit! :clap:`)
+
+        message.channel.send(`:clap: **|** :clap: ${text} :clap:`)
+	},
+};
