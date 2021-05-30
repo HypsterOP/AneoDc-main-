f49f490ef1efb568c80e17c25002a483b91580a9
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-05-30 13:38:01 +0530

    akinator bolte

---
+++ commands/fun/akinator.js
@@ -0,0 +1,16 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const akinator = require('discord.js-akinator')
+module.exports = {
+    name: 'akinator',
+    aliases: ['aki'],
+    description: 'Akinator game but on discord',
+    usage: 'akinator <option>',
+    /** 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+        akinator(message, client)
+    }
+}
