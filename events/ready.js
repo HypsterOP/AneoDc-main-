88ffaae95910a6a1da6b279d1f241d2a6aaa03ec
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-01 15:55:09 +0530

    removed ratre limt

--- events/ready.js
+++ events/ready.js
@@ -23,20 +23,12 @@ setInterval(() => {
 	})
 }, 1800000)
 
-	const hook = new dc.WebhookClient('847787309877559306', 'UrTB3GsKzJq6PIo7WZIISZKNvCYJ8u3OQBp2CEB9q14G5z5CZoNXgCXMYUEboWzZBXtj')
-
-	hook.send('<:status_online:843174802683592714> Aneo has just been restarted and is now `Online`', {
-		username: client.user.username,
-		avatarURL: client.user.displayAvatarURL()
-	})
-
 	new WokCommands(client, {
 		commandsDir: 'command',
 		testServers: [guildId],
 		showWarns: false,
 	});
 
-	client.on('error', () => hook.send(`Aneo has just encountered an error, the developers are on it! Please have some patience`))
 
 	console.log('------------------------------------------------')
 	console.log(chalk.blue(`[CLIENT] ${client.user.username} is Ready!`))
