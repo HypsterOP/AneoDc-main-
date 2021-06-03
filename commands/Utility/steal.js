1eeccfb65f645a17e2966742024dc8d91ff525ad
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-03 12:38:07 +0530

    fixed some bugsi n emoji

--- commands/Utility/steal.js
+++ commands/Utility/steal.js
@@ -1,5 +1,5 @@
-const { Client, Message, MessageEmbed, Util } = require("discord.js");
-const config = require("../../config.json")
+const Discord = require('discord.js')
+const config = require('../../config.json')
 module.exports = {
   name: 'steal',
   /**
@@ -7,19 +7,32 @@ module.exports = {
    * @param {Message} message
    * @param {String[]} args
    */
-  run: async (client, message, args) => {
-    if(!message.member.hasPermission("MANAGE_EMOJIS")) return;
-    if(!args.length) return message.channel.send('No emoji specified');
 
-    for (const rawEmoji of args) {
-        const parsedEmoji = Util.parseEmoji(rawEmoji);
-
-        if(parsedEmoji.id) {
-            const extension = parsedEmoji.animated ? ".gif" : ".png";
-            const url = `https://cdn.discordapp.com/emojis/${parsedEmoji.id + extension}`;
-            message.guild.emojis.create(url, parsedEmoji.name)
-                .then((emoji) => message.channel.send(`Added the emoji with the link: \`${emoji.url}\``))
-        }
-    }
-  },
-};
+  run: async(client , message , args) => {
+    const errembed = new Discord.MessageEmbed()
+          .setTitle(`${config.femoji}`)
+          .setDescription(`An error occured! Please check if you used the command correctly. Possible Reasons:\n\n• Correct Usage: ${await client.prefix(message)}steal (emoji name) (link)\n• File cannot be larger than 256.0 kb.\n• Invalid image`)
+          .setColor("RED")
+    if (!message.guild.me.hasPermission("MANAGE_EMOJIS")) return message.reply("I am missing the `MANAGE_EMOJIS` permission!")
+    if (!message.member.hasPermission("MANAGE_EMOJIS")) return message.reply("You are missing the `MANAGE_EMOJIS` permission!")
+ const url = args[1];
+ const name = args[0]
+        if (!name) return message.reply(`Usage: ${await client.prefix(messafe)}steal <name> <image or gif url>`)
+        if (name.length < 2 || name.length > 32) return message.reply(`\`\`\`Invalid Form Body
+name: Must be between 2 and 32 in length.\`\`\``)
+        if (!url) return message.reply(`Usage: ${await client.prefix(message)}steal <name> <image or gif url>`)
+   
+        message.guild.emojis.create(url, name)
+         
+   
+         let embed = new Discord.MessageEmbed()
+         .setTitle("Emoji Added")
+         .setDescription(`Added \`${name}\` as an emoji!`)
+         .setColor("RANDOM")
+         .setThumbnail(url)
+         .setFooter(" ")
+          message.channel.send(embed).catch(err => 
+          message.channel.send(errembed)
+          )
+  }
+}
