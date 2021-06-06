bdf25d4f9182ecc1c35b5a4752f5bfb6dc3dd04c
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-06 18:15:10 +0530

    url shortner

---
+++ commands/Utility/urlshortner.js
@@ -0,0 +1,32 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+let isgd = require('isgd')
+module.exports = {
+	name: 'url-shorten',
+	aliases: ['us'],
+	description: 'Shorten a url',
+	usage: '<url>',
+	/**
+	 * @param {Client} client
+	 * @param {Message} message
+	 * @param {String[]} args
+	 */
+	run: async(client, message, args) => {
+    if(!args[0]) return message.channel.send(`${client.error} Incorrect usage! ${await client.prefix(message)}url-shorten <url> [title]`)
+
+    if(!args[1]){
+      isgd.shorten(args[0], function(res){
+		if(res.startsWith('Error:')) return message.channel.send(`${client.error} Please enter a valid url.`)
+
+		message.channel.send(`Here is the url:\n${res}`)
+	  })
+    } else {
+
+		isgd.custom(args[0], args[1], function(res) {
+			if(res.startsWith(`Error:`)) return message.channel.send(`${client.error} An error has occured: ${res}`)
+
+			message.channel.send(`Here is the url:\n${res}`)
+		})
+
+	}
+	}
+}
