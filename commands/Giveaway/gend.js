009d2dc250fd98e03fa882c92a7e8e2d0b152325
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-11 10:04:37 +0530

    fixed giveaway problem

--- commands/Giveaway/gend.js
+++ commands/Giveaway/gend.js
@@ -4,26 +4,23 @@ const config = require("../../config.json")
 module.exports = {
     name: 'gend',
     aliases: ['end'],
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
      */
     run: async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
 
         if(!args[0]) return message.reply("You need to specify the message ID!")
 
-        let giveaway = 
-        // sdfds
-        client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) ||
-        // sdfsd
-        client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
+        if(isNaN(args[0])) return message.reply(`Message id must be a number.`)
 
-        if(!giveaway){
-            return message.channel.send('Unable to find a giveaway for `'+ args.join(' ') + '`.');
-        }
-        client.giveawaysManager.edit(giveaway.messageID, {
+        let giveaway = client.giveawaysManager.giveaways.find(
+            g => g.messageID === args[0] && g.guildID === message.guild.id
+        );
+if(!giveaway) return message.channel.send(`I was not able to find a message with that id in this server.`)
+        client.giveawaysManager.edit(giveaway.args[0], {
             setEndTimestamp: Date.now()
         })
         // sfsd againb XDDDDD
@@ -31,13 +28,13 @@ module.exports = {
             message.channel.send('Giveaway will end in less than '+(client.giveawaysManager.options.updateCountdownEvery/1000)+' seconds...');
         })
         .catch((e) => {
-            if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} is already ended.`)){
+            if(e.startsWith(`Giveaway with message ID ${giveaway.args[0]} is already ended.`)){
                 message.channel.send('This giveaway is already ended!');
             } else {
                 // console.error(e);
             message.channel.send('An error occured...');
             }
         })
-            
+
     }
 }
--- commands/Giveaway/greroll.js
+++ commands/Giveaway/greroll.js
@@ -3,34 +3,31 @@ const { Client, Message, MessageEmbed } = require('discord.js');
 module.exports = {
     name: 'greroll',
     aliases: ['reroll'],
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
      */
     run: async(client, message, args) => {
         if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
 
         if(!args[0]) return message.reply("You need to specify the message ID!")
 
-        let giveaway = 
-        client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) ||
-        // Search with giveaway ID
-        client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
+          if(isNaN(args[0])) return message.reply(`Message id must be a number.`)
 
-        // no gw
-        if(!giveaway) {
-            return message.channel.send('Unable to find a giveaway for `'+ args.join(' ') +'`.');
-        }
+        let giveaway = client.giveawaysManager.giveaways.find(
+            g => g.messageID === args[0] && g.guildID === message.guild.id
+        );
+        if(!giveaway) return message.channel.send(`I was not able to find a message with that id in this server.`)
 
         // reroll
-        client.giveawaysManager.reroll(giveaway.messageID)
+        client.giveawaysManager.reroll(giveaway.args[0])
         .then(() => {
             // Success message
             message.channel.send('Giveaway rerolled!');
         })
         .catch((e) => {
-            if(e.startsWith(`Giveaway with message ID ${giveaway.messageID} is not ended.`)){
+            if(e.startsWith(`Giveaway with message ID ${giveaway.args[0]} is not ended.`)){
                 message.channel.send('This giveaway is not ended!');
             } else {
                 // console.error(e);
