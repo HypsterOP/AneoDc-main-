0227ef7c315c68627ee450e23169757cdb5c888a
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-12 12:20:43 +0530

    uselessweb

---
+++ commands/fun/uselessweb.js
@@ -0,0 +1,18 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const axios = require('axios');
+module.exports = {
+	name: 'useless-web',
+	aliases: ['uselessweb'],
+	description: 'Shows a useless web',
+	usage: '',
+	/**
+	 * @param {Client} client
+	 * @param {Message} message
+	 * @param {String[]} args
+	 */
+	run: async (client, message, args) => {
+		let data = await axios
+			.get(`https://api.hypsterisgod.repl.co/useless-web`)
+			.then((res) => message.channel.send(res.data.url));
+	},
+};
