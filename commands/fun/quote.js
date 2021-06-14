7fba6d232496e35268b56a1076473b6ecf1901d6
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-14 17:51:15 +0530

    caps

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -18,7 +18,7 @@ module.exports = {
 
         const userAvatar = user.user.displayAvatarURL({ format: 'png' })
 
-        const img = await Canvas.quote({ image: userAvatar, message: text, username: user.username, color: color })
+        const img = await Canvas.quote({ image: userAvatar, message: text, username: user.nickname, color: color })
 
         let attachemnt = new MessageAttachment(img, "quote.png")
         message.channel.send(attachemnt)
