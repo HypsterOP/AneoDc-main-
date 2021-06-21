39aed57e77ce8d29e36e87b02ad9bc8489de1396
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-06-21 15:40:33 +0530

    change ping cmd xd

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,26 +1,16 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
-const moment = require('moment')
+
 module.exports = {
     name: 'ping',
     aliases: ['p'],
+    description: 'Shows the bot ping',
+    usage: '',
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-      const dbping = await client.db.ping();
-      const uptimed = moment
-			.duration(client.uptime)
-			.format('D [days], H [hours], m [minutes], s [seconds]');
-      message.channel.send(
-        new MessageEmbed()
-        .setTitle(`Pong!`)
-        .addFields({
-          name: `Shard ${message.guild.shardID}`,
-          value: `<:aneoTick:842630449200889856> Latency: ${Math.round(client.ws.ping)}ms\n\n🕖 Uptime: ${uptimed}\n\n<:quickmongo:847767036314779708> Database Ping: ${dbping.average}ms\n\n<:shard:847781581635846175> Shard ${message.guild.shard.id} Ping: ${message.guild.shard.ping}ms`
-        })
-        .setColor('RANDOM')
-      )
+      return message.reply(`Hello! My ping is - \`\`${client.ws.ping}\`ms`)
     }
 }
