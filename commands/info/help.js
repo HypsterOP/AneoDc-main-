447fdaadc6b25012eb2796a9e5117b0ae057f0a4
aneeshpro <76608008+aneeshpro@users.noreply.github.com>
2021-07-11 11:54:04 +0530

    open source!

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,167 +1,54 @@
-const { MessageEmbed, Message, Client } = require("discord.js");
-const { readdirSync, readdir } = require("fs");
+const { MessageEmbed } = require("discord.js");
+const { readdirSync } = require("fs");
 
 module.exports = {
   name: "help",
   aliases: ["h"],
   description: "Shows all available bot commands.",
   run: async (client, message, args) => {
-    const prefix = await client.prefix(message);
-
+    let prefix = await client.prefix(message);
     const roleColor =
       message.guild.me.displayHexColor === "#000000"
         ? "#ffffff"
         : message.guild.me.displayHexColor;
 
     if (!args[0]) {
-      const letEmbed = new MessageEmbed()
-        .setTitle(`Need help? Here are all of my commands!`)
-        .setDescription(`React with the emoji's to move the pages!`)
-        .setColor("DARK_BUT_NOT_BLACK");
+      let categories = [];
 
-      const AntiAlt = readdirSync("./commands/AntiAlt")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+      readdirSync("./commands/").forEach((dir) => {
+        const commands = readdirSync(`./commands/${dir}/`).filter((file) =>
+          file.endsWith(".js")
+        );
 
-      const Antilink = readdirSync("./commands/Antilink")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+        const cmds = commands.map((command) => {
+          let file = require(`../../commands/${dir}/${command}`);
 
-      const antiping = readdirSync("./commands/antiping")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+          if (!file.name) return "No command name.";
 
-      const antiswear = readdirSync("./commands/antiswear")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+          let name = file.name.replace(".js", "");
 
-      const Bio = readdirSync("./commands/Bio")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+          return `\`${name}\``;
+        });
 
-      const birthday = readdirSync("./commands/birthday")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+        let data = new Object();
 
-      const Bot = readdirSync("./commands/Bot")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+        data = {
+          name: dir.toUpperCase(),
+          value: cmds.length === 0 ? "In progress." : cmds.join(" "),
+        };
 
-      const customcommands = readdirSync("./commands/customcommands")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
+        categories.push(data);
+      });
 
-      const DiscordTogeht = readdirSync("./commands/DiscordTogether")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
-
-      const Economy = readdirSync("./commands/Economy")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
-
-      const fun = readdirSync("./commands/fun")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
-
-      const Giveaway = readdirSync("./commands/Giveaway")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
-
-      const info = readdirSync("./commands/info")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""));
-
-        const Leveling = readdirSync('./commands/Leveling')
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.replace(".js", ""))
-
-        const moderation = readdirSync("./commands/moderation")
-        .filter((file) => file.endsWith(".js"))
-        .map((file) => file.endsWith(".js", ""))
-
-        const ModLogs = readdirSync("./commands/ModLogs")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const Music = readdirSync("./commands/Music")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const mute = readdirSync("./commands/mute")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const Prefix = readdirSync("./commands/Prefix")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const ranks = readdirSync("./commands/ranks")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const reactionroles = readdirSync("./commands/reactionroles")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const starBoard = readdirSync("./commands/Starboard")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const suggestions = readdirSync("./commands/Suggestions")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const Tickets = readdirSync("./commands/Tickets")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const Utility = readdirSync("./commands/Utility")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const VoiceLeveling = readdirSync("./commands/Voice Leveling")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-        const WelcmoeSetup = readdirSync("./commands/welcomesetup")
-          .filter((file) => file.endsWith(".js"))
-          .map((file) => file.endsWith(".js", ""));
-
-          const ytnotifs = readdirSync("./commands/Youtube Notifications")
-            .filter((file) => file.endsWith(".js"))
-            .map((file) => file.endsWith(".js", ""));
-
-            letEmbed.addField(`Anti Alt`, AntiAlt)
-            letEmbed.addField(`Anti Link`, Antilink)
-            letEmbed.addField(`Anti Ping`, antiping);
-            letEmbed.addField(`Anti Swear`, antiswear);
-            letEmbed.addField(`Bio`, Bio);
-            letEmbed.addField(`Birthday`, birthday);
-            letEmbed.addField(`Bot`, Bot);
-            letEmbed.addField(`Custom Commands`, customcommands);
-            letEmbed.addField(`Discord Together`, DiscordTogeht);
-            letEmbed.addField(`Economy`, Economy);
-            letEmbed.addField(`Fun`, fun);
-            letEmbed.addField(`Giveaways`, Giveaway);
-            letEmbed.addField(`Info`, info);
-            letEmbed.addField(`Leveling`, Leveling);
-            letEmbed.addField(`Moderation`, moderation);
-            letEmbed.addField(`Mod Logs`, ModLogs);
-            letEmbed.addField(`Music`, Music);
-            letEmbed.addField(`Mute`, mute);
-            letEmbed.addField(`Prefix`, Prefix);
-            letEmbed.addField(`Ranks`, ranks);
-            letEmbed.addField(`Reaction Roles`, reactionroles);
-            letEmbed.addField(`Starboard`, starBoard);
-            letEmbed.addField(`Suggestions`, suggestions);
-            letEmbed.addField(`Tickets`, Tickets);
-            letEmbed.addField(`Utility`, Utility);
-            letEmbed.addField(`Voice Leveling`, VoiceLeveling);
-            letEmbed.addField(`Welcome setup`, WelcmoeSetup);
-            letEmbed.addField(`YouTube Notifications`, ytnotifs);
-
-            message.channel.send({ embeds: [letEmbed] })
-
+      const embed = new MessageEmbed()
+        .setTitle("Need help? Here are all of my commands:")
+        .addFields(categories)
+        .setDescription(
+          `Use \`${prefix}help\` followed by a command name to get more additional information on a command. For example: \`${prefix}help ban\`.`
+        )
+        .setTimestamp()
+        .setColor(roleColor);
+      return message.channel.send({embeds: [embed]});
     } else {
       const command =
         client.commands.get(args[0].toLowerCase()) ||
@@ -175,7 +62,7 @@ module.exports = {
             `Invalid command! Use \`${prefix}help\` for all of my commands!`
           )
           .setColor("FF0000");
-        return message.channel.send({ embeds: [embed] });
+        return message.channel.send({embeds: [embed]});
       }
 
       const embed = new MessageEmbed()
@@ -209,7 +96,7 @@ module.exports = {
         )
         .setTimestamp()
         .setColor(roleColor);
-      return message.channel.send({ embeds: [embed] });
+      return message.channel.send({embeds: [embed]});
     }
   },
 };
--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,6 @@ const api = new Topgg.Api(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgxMTI2NTE5NTE4Njk3ODgyOCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjIyMjY3NDI4fQ.7SRva33Av2xaB1_jZ4PSaedpYn_H9rzsO52EKjucvds"
 );
 const WokCommands = require("wokcommands");
-// https://discord.com/api/webhooks/847787309877559306/UrTB3GsKzJq6PIo7WZIISZKNvCYJ8u3OQBp2CEB9q14G5z5CZoNXgCXMYUEboWzZBXtj
 const Schema1 = require("../models/blacklist-word");
 const { BlacklistedWords } = require("../Collection");
 const guildId = "814117890701787157";
