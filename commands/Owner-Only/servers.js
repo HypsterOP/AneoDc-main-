e626b7e2e28bef1d8abcdca8067dfdc2cad45cfb
Jambang J <70057710+jiopolm@users.noreply.github.com>
2021-07-25 11:05:14 +0530

    Update servers.js

--- commands/Owner-Only/servers.js
+++ commands/Owner-Only/servers.js
@@ -9,10 +9,10 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    if (message.author.id !== "800331322089537538") return;
+    if (message.author.id !== "800331322089537538") return; //You can change the I'D to you if you want to
     message.channel.send({content:
-      `Hello Hypster, Im in ${client.guilds.cache.size} servers`
+      `Hello Hypster, Im in ${client.guilds.cache.size} servers` //you can edit this message
     }
     );
   },
-};
+}
